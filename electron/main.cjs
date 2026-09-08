0465a9ef1ba7d9b24f07d250722d23fab76a3934
Daniel Nunes <daniel.pvn@hotmail.com>
2026-09-08 13:46:28 -0300

    feat: add desktop app support with auto-start, tray icon and minimalist logo

---
+++ electron/main.cjs
@@ -0,0 +1,150 @@
+const { app, BrowserWindow, Tray, Menu, globalShortcut, ipcMain } = require('electron');
+const path = require('path');
+
+let mainWindow = null;
+let tray = null;
+let isQuitting = false;
+
+const isDev = process.env.NODE_ENV === 'development' || !app.isPackaged;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1150,
+    height: 780,
+    minWidth: 860,
+    minHeight: 620,
+    backgroundColor: '#090a0f',
+    title: 'Planner Minimalista',
+    autoHideMenuBar: true,
+    show: false,
+    icon: path.join(__dirname, '../public/icon.svg'),
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.cjs'),
+    },
+  });
+
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:5173');
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  // Minimize to tray instead of closing completely
+  mainWindow.on('close', (event) => {
+    if (!isQuitting) {
+      event.preventDefault();
+      mainWindow.hide();
+    }
+  });
+}
+
+function createTray() {
+  try {
+    const iconPath = path.join(__dirname, '../public/icon.svg');
+    tray = new Tray(iconPath);
+    tray.setToolTip('Planner Minimalista');
+
+    const updateContextMenu = () => {
+      const isAutoStart = app.getLoginItemSettings().openAtLogin;
+
+      const contextMenu = Menu.buildFromTemplate([
+        {
+          label: 'Abrir Planner',
+          click: () => {
+            if (mainWindow) {
+              mainWindow.show();
+              mainWindow.focus();
+            }
+          },
+        },
+        {
+          label: 'Iniciar com o Windows',
+          type: 'checkbox',
+          checked: isAutoStart,
+          click: (menuItem) => {
+            app.setLoginItemSettings({
+              openAtLogin: menuItem.checked,
+              path: app.getPath('exe'),
+            });
+            updateContextMenu();
+          },
+        },
+        { type: 'separator' },
+        {
+          label: 'Sair do Planner',
+          click: () => {
+            isQuitting = true;
+            app.quit();
+          },
+        },
+      ]);
+
+      tray.setContextMenu(contextMenu);
+    };
+
+    updateContextMenu();
+
+    tray.on('double-click', () => {
+      if (mainWindow) {
+        if (mainWindow.isVisible()) {
+          mainWindow.hide();
+        } else {
+          mainWindow.show();
+          mainWindow.focus();
+        }
+      }
+    });
+  } catch (err) {
+    console.error('Falha ao criar System Tray:', err);
+  }
+}
+
+// Register single instance lock
+const gotTheLock = app.requestSingleInstanceLock();
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on('second-instance', () => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.show();
+      mainWindow.focus();
+    }
+  });
+
+  app.whenReady().then(() => {
+    createWindow();
+    createTray();
+
+    // Global shortcut Ctrl+Alt+P to quick summon the planner
+    globalShortcut.register('CommandOrControl+Alt+P', () => {
+      if (mainWindow) {
+        if (mainWindow.isVisible() && !mainWindow.isMinimized()) {
+          mainWindow.hide();
+        } else {
+          mainWindow.show();
+          mainWindow.focus();
+        }
+      }
+    });
+
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+  });
+}
+
+// Clean exit
+app.on('before-quit', () => {
+  isQuitting = true;
+});
+
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll();
+});
